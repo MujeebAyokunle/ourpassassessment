3a4de82ff6768947dc607c9b0580cdb7be41155c
MujeebAyokunle <alabimujeebayokunle@gmail.com>
2023-03-14 16:41:13 +0100

    first commit

---
+++ src/components/NavWrapper/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar({ children }) {
+  return (
+    <div>
+      <p>This is cool</p>
+      {children}
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/Invoices/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavWrapper from '../../components/NavWrapper'
+
+function Invoices() {
+    return (
+        <div>
+            <NavWrapper >
+                <p>This is inner cool</p>
+            </NavWrapper>
+        </div>
+    )
+}
+
+export default Invoices
